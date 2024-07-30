ccccb95bec376efeb87a3419c27b086c822635ba
manu31shukla <manu31shukla@gmail.com>
2024-07-30 18:32:40 +0530

    setting up 3d canvas using three js fiber and adding loader using suspense wrapper

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Html } from '@react-three/drei'
+
+const Loader = () => {
+  return (
+   <Html>
+    <div className='flex justify-center items-center'>
+        <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500'></div>
+    </div>
+    </Html>
+  )
+}
+
+export default Loader
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React , { Suspense }from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-screen relative'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+      POPUP
+
+      </div> */}
+      <Canvas className='w-full h-screen relative' camera={{near:0.1, far:100}}>
+      <Suspense fallback={<Loader/>}>
+      </Suspense>
+      </Canvas>
+
+    </section>
   )
 }
 
