ca34f3a421e16e1cc66d9bca3cd0813d32c778ba
manu31shukla <manu31shukla@gmail.com>
2024-07-30 17:57:18 +0530

    setting up routing congig

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <main className='bg-slate-300.20'>
+        <Router>
+          <Routes>
+            <Route path='/' element={'Home'} />
+            <Route path='/about' element={'About'} />
+            <Route path='/projects' element={'Projects'} />
+            <Route path='/contact' element={'Contact'} />
+          </Routes>
+        </Router>
+      </main>
     </div>
   )
 }
