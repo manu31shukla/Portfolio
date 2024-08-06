c699214f5882eae9d1d8f7b64fee8d73d8cdcc1c
manu31shukla <manu31shukla@gmail.com>
2024-08-06 18:23:03 +0530

    minimising color inconsistencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <header className='header'>
         <NavLink to="/" className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font shadow-md'>
-        <p className='blue-gradient_text'>MS</p>
+        <p className='brown-gradient_text'>MS</p>
         </NavLink>
         <nav className='flex text-lg gap-4 font-medium'>
-        <NavLink to='/about' className={({ isActive}) => isActive? 'text-blue-500' : 'text-black'}>About</NavLink>
-        <NavLink to='/projects' className={({ isActive}) => isActive? 'text-blue-500' : 'text-black'}>Projects</NavLink>
-        <NavLink to='/contact' className={({ isActive}) => isActive? 'text-blue-500' : 'text-black'}>Contact</NavLink>
+        <NavLink to='/about' className={({ isActive}) => isActive? 'text-brown' : 'text-black'}>About</NavLink>
+        <NavLink to='/projects' className={({ isActive}) => isActive? 'text-brown' : 'text-black'}>Projects</NavLink>
+        <NavLink to='/contact' className={({ isActive}) => isActive? 'text-brown' : 'text-black'}>Contact</NavLink>
         </nav>
       
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
       <Canvas className='w-full h-screen relative' camera={{near:0.1, far:100}}>
         <Suspense fallback={<Loader/>}>
           <directionalLight castShadow intensity={0.5} position={[10, 20, 10]} shadow-mapSize-width={2048}   shadow-mapSize-height={2048}/>
-          <ambientLight intensity={0.3} />
+          <ambientLight intensity={0.5} />
           <pointLight intensity={0.8} position={[10, 10, 10]} />
           <spotLight intensity={1} position={[0, 20, 0]} angle={0.3} penumbra={1} castShadow shadow-mapSize-width={2048} shadow-mapSize-height={2048} />
           <hemisphereLight skyColor={'#ffffff'} groundColor={'#000000'} intensity={0.6} />
