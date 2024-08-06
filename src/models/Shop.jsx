79024fed6d4e512c16e4021ca3233569c8c5731b
manu31shukla <manu31shukla@gmail.com>
2024-08-06 20:11:11 +0530

    rotation logic for shop

--- src/models/Shop.jsx
+++ src/models/Shop.jsx
@@ -22,6 +22,55 @@ const Shop = ({scale, position, rotation, isRotating, setIsRotating, ...props}) 
   const lastX = useRef(0);
   const rotationSpeed = useRef(0);
   const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) => {
+    e.stopPropagation(); //mouse click will only affect the clicked object
+    e.preventDefault(); 
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+  }
+
+  const handlePointerUp = (e) => {
+    e.stopPropagation(); 
+    e.preventDefault(); 
+    setIsRotating(false);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    const delta = (clientX - lastX.current)/viewport.width;
+
+    shopRef.current.rotation.y += delta * Math.PI * 2;
+
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  }
+
+  const handlePointerMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault(); 
+
+    if(isRotating) {
+      handlePointerUp(e);
+  }
+}
+
+useEffect(() => {
+  document.addEventListener('pointerdown', handlePointerDown);
+  document.addEventListener('pointerup', handlePointerUp);
+  document.addEventListener('pointermove', handlePointerMove);
+
+  return () => {
+    document.removeEventListener('pointerdown', handlePointerDown);
+    document.removeEventListener('pointerup', handlePointerUp);
+    document.removeEventListener('pointermove', handlePointerMove);
+  }
+},
+[gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
+
   return (
 <a.group ref={shopRef} scale={scale} position={position} {...props}>
       <group scale={0.01}>
